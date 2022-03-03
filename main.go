48b65930d49fdcef8536824871fc8c550b1e8c11
guanyang <yang.guan@ly.com>
2022-03-03 14:22:49 +0800

    dtm http sage init

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/dtm-labs/dtm/dtmutil"
-	"github.com/dtm-labs/github.com/guanyang-lab/dtm/dtmcli"
 	"github.com/gin-gonic/gin"
+	"github.com/guanyang-lab/dtm/dtmcli"
 )
 
 func main() {
